a14d088208b42163ef971593e02fa119f4168ff1
anacampaner <ana.fsilva@sptech.school>
2024-11-07 18:25:21 -0300

    Resolução do exercicio

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,62 @@
 package school.sptech;
 
-public class Imagem {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Imagem
+{
+    private List<Figura> figuras;
+
+    public List<Figura> getFiguras()
+    {
+        return figuras;
+    }
+    public void setFiguras(List<Figura> figuras)
+    {
+        this.figuras = figuras;
+    }
+
+    public void adicionar(Figura figura)
+    {
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas()
+    {
+        Double soma = 0.0;
+        for (Figura figura : figuras)
+        {
+            soma += figura.calcularArea();
+        }
+        return soma;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20()
+    {
+        List<Figura> figurasMaior20 = new ArrayList<>();
+
+        for(Figura figura : figuras)
+        {
+            if(figura.calcularArea() > 20)
+            {
+                figurasMaior20.add(figura);
+            }
+        }
+        return figurasMaior20;
+    }
+
+    public List<Figura> buscarQuadrados()
+    {
+        List<Figura> quadrados = new ArrayList<>();
+
+        for(Figura figura : figuras)
+        {
+            if (figura instanceof Quadrado)
+
+            {
+                quadrados.add(figura);
+            }
+        }
+        return quadrados;
+    }
 }
--- src/main/java/school/sptech/Triangulo.java
+++ src/main/java/school/sptech/Triangulo.java
@@ -1,4 +1,37 @@
 package school.sptech;
 
-public class Triangulo {
+public class Triangulo extends Figura
+{
+    private Double altura;
+    private Double base;
+
+    public Double getAltura()
+    {
+        return altura;
+    }
+    public void setAltura(Double altura)
+    {
+        this.altura = altura;
+    }
+
+    public Double getBase()
+    {
+        return base;
+    }
+    public void setBase(Double base)
+    {
+        this.base = base;
+    }
+
+    @Override
+    public Double calcularArea()
+    {
+       return (base*altura)/2;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Triângulo{" + "base='" + base + "altura='" + altura + '}';
+    }
 }
